8fd9232f79dba6fda40bbea7240d67edb16c0705
aaron.harpt <aaron.harpt@gmail.com>
2021-01-25 11:12:29 -0600

    Added scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-      System.out.println("Hello World");
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("What is your name?");
+
+      scanner.close();
     }
 }
